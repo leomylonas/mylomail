88d7d3e9cc2624a17af4a8aa423d90928699f70c
Leo Mylonas <leo@mylonas.co>
2026-09-01 08:31:29 +1000

    test(caldav): cover credential slot resolution

---
+++ server/MyloMail.Api.Tests/Providers/CalDavRequestFactoryTests.cs
@@ -0,0 +1,54 @@
+using MyloMail.Api.Credentials;
+using MyloMail.Api.Domain;
+using MyloMail.Api.Providers;
+using MyloMail.Api.Providers.CalDav;
+using Xunit;
+
+namespace MyloMail.Api.Tests.Providers;
+
+public sealed class CalDavRequestFactoryTests
+{
+	[Fact]
+	public async Task Reuse_reads_the_primary_imap_credential()
+	{
+		var account = Account(CredentialSource.ReuseImap);
+		var store = new InMemoryCredentialStore();
+		await store.StoreAsync(account.Id, new CredentialPayload(MailProviderFactory.ImapPasswordFormat, "imap-secret"u8.ToArray()), default);
+
+		using var request = await new CalDavRequestFactory(store).CreateAsync(account, HttpMethod.Get);
+
+		Assert.Equal("Basic", request.Headers.Authorization!.Scheme);
+		Assert.Equal("caldav-user:imap-secret", Decode(request));
+	}
+
+	[Fact]
+	public async Task Independent_credential_reads_only_the_caldav_slot()
+	{
+		var account = Account(CredentialSource.Independent);
+		var store = new InMemoryCredentialStore();
+		await store.StoreSlotAsync(account.Id, CredentialSlots.CalDav, new CredentialPayload(CalDavRequestFactory.PasswordFormat, "caldav-secret"u8.ToArray()), default);
+
+		using var request = await new CalDavRequestFactory(store).CreateAsync(account, HttpMethod.Get);
+
+		Assert.Equal("caldav-user:caldav-secret", Decode(request));
+	}
+
+	private static Account Account(CredentialSource source) => new()
+	{
+		Id = Guid.NewGuid(),
+		ProviderType = ProviderType.Imap,
+		ProviderConfig = new ImapProviderConfig
+		{
+			CalDav = new CalDavProviderConfig
+			{
+				Endpoint = "https://calendar.example.test/dav/",
+				UserName = "caldav-user",
+				CredentialSource = source,
+			},
+		},
+	};
+
+	private static string Decode(HttpRequestMessage request) => System.Text.Encoding.UTF8.GetString(
+		Convert.FromBase64String(request.Headers.Authorization!.Parameter!)
+	);
+}
